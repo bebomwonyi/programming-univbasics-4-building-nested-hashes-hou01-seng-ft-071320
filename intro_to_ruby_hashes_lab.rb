297ce117a7965016803acba40f6f93f0147a157d
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-06 14:35:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- {railroads:{
+ monopoly = {railroads:{
    pieces: 4 ,
    rent_in_dollars: {
      one_piece_owned: 25, two_piece_owned: 50, three_piece_owned: 100,  four_piece_owned: 200},
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
- {
+ mo{
    railroads:
    {
      pieces: 4 ,rent_in_dollars: 
