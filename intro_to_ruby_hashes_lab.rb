14a1cf401eaf518422464ea32b378b31e5d7f5e8
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-06 14:42:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,7 @@ end
 
 def monopoly_with_second_tier
  monopoly = {
-   railroads:""
-   {
+   railroads:{
      pieces: 4 ,
      rent_in_dollars: "",
      names: ""
@@ -16,8 +15,7 @@ end
 
 def monopoly_with_third_tier
  monopoly = {
-   railroads:
-   {
+   railroads:{
    pieces: 4 ,
    rent_in_dollars: 
    {
@@ -39,8 +37,7 @@ end
 
 def monopoly_with_fourth_tier
  monopoly = {
-   railroads:
-   {
+   railroads:{
      pieces: 4 ,rent_in_dollars: 
      {
        one_piece_owned: 25,
