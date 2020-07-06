cfe7780d746bf1f21647c7a27ea84b05e0299236
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-06 14:23:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_third_tier
    rent_in_dollars: {
      one_piece_owned: "", two_piece_owned: "", three_piece_owned: "",  four_piece_owned: ""},
    names:{
-     
+     reading_railroad: "",
    }
    }
  }
