e3010124ec3e638cc8a02040088e2e4b8d7fdb82
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-06 14:51:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,10 @@ def monopoly_with_third_tier
    railroads:{
    pieces: 4 ,
    rent_in_dollars:{
-     one_piece_owned: 25,
-     two_piece_owned: 50,
-     three_piece_owned: 100,
-     four_piece_owned: 200},
+     one_pieces_owned: 25,
+     two_pieces_owned: 50,
+     three_pieces_owned: 100,
+     four_pieces_owned: 200},
    names:{
      reading_railroad: "",
      pennsylvania_railroad: "",
@@ -32,10 +32,10 @@ def monopoly_with_fourth_tier
    railroads:{
      pieces: 4 ,
      rent_in_dollars:{
-       one_piece_owned: 25,
-       two_piece_owned: 50,
-       three_piece_owned: 100,
-       four_piece_owned: 200},
+       one_pieces_owned: 25,
+       two_pieces_owned: 50,
+       three_pieces_owned: 100,
+       four_pieces_owned: 200},
      names:{
        reading_railroad:{
          "mortgage_value": "$100"},
